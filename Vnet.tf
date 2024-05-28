cbe85f0a629da1880783fba9e78529c6dab5d599
Chinmay1997 <chinmay.dhumal@gmail.com>
2024-05-28 12:19:00 +0530

    Update Vnet.tf

--- Vnet.tf
+++ Vnet.tf
@@ -5,6 +5,19 @@ resource "azurerm_virtual_network" "default" {
   resource_group_name = azurerm_resource_group.mediawiki.name
 }
 
+resource "azurerm_route_table" "vnet_priv_rt" {
+  name                          = "vnet_priv_rt"
+  location                      = azurerm_resource_group.mediawiki.location
+  resource_group_name           = azurerm_resource_group.mediawiki.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "internal route"
+    address_prefix = "10.0.0.0/16"
+    next_hop_type  = "VnetLocal"
+  }
+}
+
 resource "azurerm_subnet" "dbsubnet" {
   address_prefixes     = ["10.0.2.0/24"]
   name                 = "subnet-mediawiki-db"
@@ -31,6 +44,25 @@ resource "azurerm_subnet" "compute" {
   virtual_network_name = azurerm_virtual_network.default.name
   service_endpoints    = ["Microsoft.Storage"]
 }
+
+resource "azurerm_subnet" "public_subnet" {
+  address_prefixes     = ["10.0.3.0/24"]
+  name                 = "public_subnet"
+  resource_group_name  = azurerm_resource_group.mediawiki.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  service_endpoints    = ["Microsoft.Storage"]
+}
+
+resource "azurerm_subnet_route_table_association" "db_subnet" {
+  subnet_id      = azurerm_subnet.compute.id
+  route_table_id = azurerm_route_table.vnet_priv_rt.id
+}
+
+resource "azurerm_subnet_route_table_association" "compute_subnet" {
+  subnet_id      = azurerm_subnet.dbsubnet.id
+  route_table_id = azurerm_route_table.vnet_priv_rt.id
+}
+
 
 resource "azurerm_private_dns_zone" "default" {
   name                = "mediawiki.mysql.database.azure.com"
